1df3b2be49d5bf9f178e3f9e4ff6600e10317345
Oskar Schöldström <m@oxy.fi>
2017-09-20 16:05:27 -0500

    limit post revisions to 5

--- config/application.php
+++ config/application.php
@@ -97,6 +97,7 @@ define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_EDIT', true);
 define('WPCACHEHOME', WP_CONTENT_DIR . '/plugins/wp-super-cache/');
 define('WP_HIDE_DONATION_BUTTONS', true);
+define('WP_POST_REVISIONS', 5);
 
 /**
  * Bootstrap WordPress
