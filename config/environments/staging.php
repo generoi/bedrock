5da269dde5996a78fafa0742abb344b6e22d2b73
Oskar Schöldström <m@oxy.fi>
2018-01-24 13:07:43 -0300

    Set hardcoded wp-mail-smtp settings

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,3 +5,7 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+
+/** WP Mail SMTP hardcoded configuration */
+define('WPMS_ON', true);
+define('WPMS_MAILER', 'mail');
