2ab52f79c7600a8feb5b58e277448df959fbd893
Oskar Schöldström <m@oxy.fi>
2018-01-24 13:07:43 -0300

    Set hardcoded wp-mail-smtp settings

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,3 +6,7 @@ define('SCRIPT_DEBUG', true);
 define('WPML_DEBUG_INSTALLER', true);
 define('WP_DEBUG_LOG', true);
 define('WP_CACHE', false);
+
+/** WP Mail SMTP hardcoded configuration */
+define('WPMS_ON', true);
+define('WPMS_MAILER', 'mail');
--- config/environments/production.php
+++ config/environments/production.php
@@ -6,3 +6,11 @@ define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
 define('WP_CACHE', true);
+
+/** WP Mail SMTP hardcoded configuration */
+define('WPMS_ON', true);
+define('WPMS_MAILER', 'smtp');
+define('WPMS_SMTP_HOST', 'smtp.multi.fi');
+define('WPMS_SMTP_PORT', 465);
+define('WPMS_SSL', 'ssl');
+define('WPMS_SMTP_AUTH', false);
