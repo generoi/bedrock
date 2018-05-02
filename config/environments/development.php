3ecccb4e5dc8b9003ad19b01679c94863c0568eb
Oskar Schöldström <m@oxy.fi>
2018-05-02 11:27:11 -0300

    allow database repair access in development mode

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,6 +6,8 @@ define('SCRIPT_DEBUG', true);
 define('WPML_DEBUG_INSTALLER', true);
 define('WP_DEBUG_LOG', true);
 define('WP_CACHE', false);
+/** Access /wp/wp-admin/maint/repair.php **/
+define('WP_ALLOW_REPAIR', true);
 
 /** WP Mail SMTP hardcoded configuration */
 define('WPMS_ON', true);
