1505cfb1942658992a144b724d78e9869d23badf
Oskar Schöldström <m@oxy.fi>
2019-06-10 19:12:20 -0300

    add show-environment-in-admin-bar

--- config/environments/development.php
+++ config/environments/development.php
@@ -13,6 +13,7 @@ Config::define('WPML_DEBUG_INSTALLER', true);
 Config::define('WP_DEBUG_LOG', true);
 Config::define('WP_CACHE', false);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SHC_SHOW_ENV_DEV', 'dev');
 
 /** Access /wp/wp-admin/maint/repair.php **/
 Config::define('WP_ALLOW_REPAIR', true);
