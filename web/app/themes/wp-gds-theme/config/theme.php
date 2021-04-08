dadbe913d3457e064fa476cdc728526e2235a890
Oskar Schöldström <public@oxy.fi>
2021-04-08 15:16:41 -0300

    Use gds variables in Gutenberg rather than other way around

--- web/app/themes/wp-gds-theme/config/theme.php
+++ web/app/themes/wp-gds-theme/config/theme.php
@@ -5,106 +5,106 @@ return [
         [
             'name'  => __('White', 'gds'),
             'slug'  => 'white',
-            'color' => '#fff',
+            'color' => 'var(--color-white)',
         ],
         [
             'name'  => __('Light gray', 'gds'),
             'slug'  => 'ui-background-01',
-            'color' => '#eee',
+            'color' => 'var(--color-ui-background-01)',
         ],
         [
             'name'  => __('Light gray 50%', 'gds'), // @todo style guide has outdated value
             'slug'  => 'ui-background-02',
-            'color' => 'rgb(238 238 238 / 50%)',
+            'color' => 'var(--color-ui-background-02)',
         ],
         [
             'name'  => __('Medium gray', 'gds'),
             'slug'  => 'ui-01',
-            'color' => '#acacac',
+            'color' => 'var(--color-ui-01)',
         ],
         [
             'name'  => __('Dark gray', 'gds'),
             'slug'  => 'ui-02',
-            'color' => '#646464',
+            'color' => 'var(--color-ui-02)',
         ],
         [
             'name'  => __('Black', 'gds'),
             'slug'  => 'black',
-            'color' => '#333031',
+            'color' => 'var(--color-black)',
         ],
         [
             'name'  => __('Green', 'gds'),
             'slug'  => 'ui-03',
-            'color' => '#00a06e',
+            'color' => 'var(--color-ui-03)',
         ],
         [
             'name'  => __('Blue', 'gds'),
             'slug'  => 'ui-04',
-            'color' => '#00a3b7',
+            'color' => 'var(--color-ui-04)',
         ],
         [
             'name'  => __('Red', 'gds'),
             'slug'  => 'ui-05',
-            'color' => '#f1615e',
+            'color' => 'var(--color-ui-05)',
         ],
         [
             'name'  => __('Pink', 'gds'),
             'slug'  => 'ui-06',
-            'color' => '#ffdcdd',
+            'color' => 'var(--color-ui-06)',
         ],
         [
             'name'  => __('Purple', 'gds'),
             'slug'  => 'ui-07',
-            'color' => '#9185db',
+            'color' => 'var(--color-ui-07)',
         ],
     ],
     'editor-font-sizes' => [
         [
             'name' => __('S paragraph', 'gds'),
             'slug' => 's-paragraph',
-            'size' => 15,
+            'size' => 'var(--paragraph-s-font-size)',
         ],
         [
             'name' => __('M paragraph', 'gds'),
             'slug' => 'm-paragraph',
-            'size' => 17,
+            'size' => 'var(--paragraph-m-font-size)',
         ],
         [
             'name' => __('L paragraph', 'gds'),
             'slug' => 'l-paragraph',
-            'size' => 20,
+            'size' => 'var(--paragraph-l-font-size)',
         ],
         [
             'name' => __('S heading', 'gds'),
             'slug' => 's-heading',
-            'size' => 19,
+            'size' => 'var(--heading-s-font-size)',
         ],
         [
             'name' => __('M heading', 'gds'),
             'slug' => 'm-heading',
-            'size' => 20,
+            'size' => 'var(--heading-m-font-size)',
         ],
         [
             'name' => __('L heading', 'gds'),
             'slug' => 'l-heading',
-            'size' => 36,
+            'size' => 'var(--heading-l-font-size)',
         ],
         [
             'name' => __('XL heading', 'gds'),
             'slug' => 'xl-heading',
-            'size' => 60,
+            'size' => 'var(--heading-xl-font-size)',
         ],
         [
             'name' => __('XXL heading', 'gds'),
             'slug' => 'xxl-heading',
-            'size' => 62,
+            'size' => 'var(--heading-xxl-font-size)',
         ],
     ],
     'editor-gradient-presets' => [
         [
             'name'     => __('Green radial gradient', 'gds'),
-            'slug'     => 'block-01',
-            'gradient' => 'radial-gradient(closest-side, #000000, #ffffff)',
+            'slug'     => 'gradient-01',
+            'gradient' => 'var(--gradient-01)',
         ],
     ],
 ];
