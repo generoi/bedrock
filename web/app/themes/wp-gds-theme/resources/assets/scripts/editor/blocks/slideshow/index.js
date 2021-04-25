7b2c2467f7fd6a8d02d4196cfeb5fc4707517b5e
Oskar Schöldström <public@oxy.fi>
2021-04-25 14:09:56 -0300

    Update to laravel mix v6

--- web/app/themes/wp-gds-theme/resources/assets/scripts/editor/blocks/slideshow/index.js
+++ web/app/themes/wp-gds-theme/resources/assets/scripts/editor/blocks/slideshow/index.js
@@ -4,16 +4,13 @@ import { registerBlockType } from '@wordpress/blocks'
 import { InnerBlocks } from '@wordpress/block-editor'
 
 import edit from './edit'
-import { name, category, supports, attributes, apiVersion } from './block.json';
+import meta from './block.json';
 
-registerBlockType(name, {
-  apiVersion,
+registerBlockType(meta.name, {
+  ...meta,
   title: __('Slideshow'),
   description: __('Display content in a slideshow'),
   icon: 'image-flip-horizontal',
-  category,
-  supports,
-  attributes,
   edit,
   save() {
     return <InnerBlocks.Content />;
