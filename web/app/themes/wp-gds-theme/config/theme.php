51b0b0244a274cb669a6937365908f4e1a843fd6
Oskar Schöldström <public@oxy.fi>
2021-04-25 20:09:47 -0300

    Update GDS to v4.0.0-beta

--- web/app/themes/wp-gds-theme/config/theme.php
+++ web/app/themes/wp-gds-theme/config/theme.php
@@ -5,99 +5,99 @@ return [
         [
             'name'  => __('White', 'gds'),
             'slug'  => 'white',
-            'color' => 'var(--color-white)',
+            'color' => 'var(--gds-color-white)',
         ],
         [
             'name'  => __('Light gray', 'gds'),
             'slug'  => 'ui-background-01',
-            'color' => 'var(--color-ui-background-01)',
+            'color' => 'var(--gds-color-ui-background-01)',
         ],
         [
             'name'  => __('Light gray 50%', 'gds'), // @todo style guide has outdated value
             'slug'  => 'ui-background-02',
-            'color' => 'var(--color-ui-background-02)',
+            'color' => 'var(--gds-color-ui-background-02)',
         ],
         [
             'name'  => __('Medium gray', 'gds'),
             'slug'  => 'ui-01',
-            'color' => 'var(--color-ui-01)',
+            'color' => 'var(--gds-color-ui-01)',
         ],
         [
             'name'  => __('Dark gray', 'gds'),
             'slug'  => 'ui-02',
-            'color' => 'var(--color-ui-02)',
+            'color' => 'var(--gds-color-ui-02)',
         ],
         [
             'name'  => __('Black', 'gds'),
             'slug'  => 'black',
-            'color' => 'var(--color-black)',
+            'color' => 'var(--gds-color-black)',
         ],
         [
             'name'  => __('Green', 'gds'),
             'slug'  => 'ui-03',
-            'color' => 'var(--color-ui-03)',
+            'color' => 'var(--gds-color-ui-03)',
         ],
         [
             'name'  => __('Blue', 'gds'),
             'slug'  => 'ui-04',
-            'color' => 'var(--color-ui-04)',
+            'color' => 'var(--gds-color-ui-04)',
         ],
         [
             'name'  => __('Red', 'gds'),
             'slug'  => 'ui-05',
-            'color' => 'var(--color-ui-05)',
+            'color' => 'var(--gds-color-ui-05)',
         ],
         [
             'name'  => __('Pink', 'gds'),
             'slug'  => 'ui-06',
-            'color' => 'var(--color-ui-06)',
+            'color' => 'var(--gds-color-ui-06)',
         ],
         [
             'name'  => __('Purple', 'gds'),
             'slug'  => 'ui-07',
-            'color' => 'var(--color-ui-07)',
+            'color' => 'var(--gds-color-ui-07)',
         ],
     ],
     'editor-font-sizes' => [
         [
             'name' => __('S paragraph', 'gds'),
             'slug' => 's-paragraph',
-            'size' => 'var(--paragraph-s-font-size)',
+            'size' => 'var(--gds-paragraph-s-font-size)',
         ],
         [
             'name' => __('M paragraph', 'gds'),
             'slug' => 'm-paragraph',
-            'size' => 'var(--paragraph-m-font-size)',
+            'size' => 'var(--gds-paragraph-m-font-size)',
         ],
         [
             'name' => __('L paragraph', 'gds'),
             'slug' => 'l-paragraph',
-            'size' => 'var(--paragraph-l-font-size)',
+            'size' => 'var(--gds-paragraph-l-font-size)',
         ],
         [
             'name' => __('S heading', 'gds'),
             'slug' => 's-heading',
-            'size' => 'var(--heading-s-font-size)',
+            'size' => 'var(--gds-heading-s-font-size)',
         ],
         [
             'name' => __('M heading', 'gds'),
             'slug' => 'm-heading',
-            'size' => 'var(--heading-m-font-size)',
+            'size' => 'var(--gds-heading-m-font-size)',
         ],
         [
             'name' => __('L heading', 'gds'),
             'slug' => 'l-heading',
-            'size' => 'var(--heading-l-font-size)',
+            'size' => 'var(--gds-heading-l-font-size)',
         ],
         [
             'name' => __('XL heading', 'gds'),
             'slug' => 'xl-heading',
-            'size' => 'var(--heading-xl-font-size)',
+            'size' => 'var(--gds-heading-xl-font-size)',
         ],
         [
             'name' => __('XXL heading', 'gds'),
             'slug' => 'xxl-heading',
-            'size' => 'var(--heading-xxl-font-size)',
+            'size' => 'var(--gds-heading-2xl-font-size)',
         ],
     ],
     'editor-gradient-presets' => [
