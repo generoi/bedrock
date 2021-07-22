f240325310aa9a2f0aa421f89be43ae81c664172
Oskar Schöldström <public@oxy.fi>
2021-07-22 11:03:12 -0300

    Move disable-weak-password js into php file so its self contained

---
+++ web/app/mu-plugins/disable-password-change-emails.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Plugin Name:  Disable Admin Notification of User Password Change
+Plugin URI:   https://genero.fi
+Description:  Various defaults for WP
+Version:      1.0.0
+Author:       Genero
+Author URI:   https://genero.fi/
+License:      MIT License
+*/
+
+namespace Genero\Site;
+
+if (!is_blog_installed()) {
+    return;
+}
+
+/**
+ * Disable Admin Notification of User Password Change
+ *
+ * @see pluggable.php
+ */
+if (!function_exists('wp_password_change_notification')) {
+    function wp_password_change_notification($user)
+    {
+        return;
+    }
+}
