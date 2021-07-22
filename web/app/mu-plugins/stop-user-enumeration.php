3e42f6f744293b3a8fff78aa32b7014490dc2793
Oskar Schöldström <public@oxy.fi>
2021-07-22 11:02:57 -0300

    Prevent user enumeration using login/lost-password confirmations

--- web/app/mu-plugins/stop-user-enumeration.php
+++ web/app/mu-plugins/stop-user-enumeration.php
@@ -12,6 +12,8 @@ License:      MIT License
 
 namespace Genero\Site;
 
+use WP_Error;
+
 if (!is_blog_installed()) {
     return;
 }
@@ -35,3 +37,32 @@ add_action('init', function () {
         return $query_vars;
     });
 });
+
+/**
+ * Do not reveal if username exists when a login fails.
+ */
+add_filter('login_errors', function ($message) {
+    global $errors;
+
+    if (isset($errors->errors['invalid_username']) || isset($errors->errors['incorrect_password'])) {
+        $message = sprintf(
+            __('<strong>ERROR</strong>: Invalid username/password combination. <a href="%1$s" title="%2$s">%3$s</a>?'),
+            site_url('wp-login.php?action=lostpassword', 'login'),
+            __('Password Lost and Found', 'default'),
+            __('Lost Password', 'default')
+        );
+    }
+
+    return $message;
+});
+
+/**
+ * Do not reveal if username exists when using lost password feature.
+ */
+add_action('lost_password', function (WP_Error $errors) {
+    // Always redirect even if there are errors.
+    if ($errors->has_errors()) {
+        $redirect_to = !empty($_REQUEST['redirect_to']) ? $_REQUEST['redirect_to'] : 'wp-login.php?checkemail=confirm';
+        wp_safe_redirect($redirect_to);
+    }
+}, 10, 2);
