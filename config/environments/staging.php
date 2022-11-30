4b283c42515e0ed25d98663b693584072ff68c64
Shayan Abbas <shayanabbas@outlook.com>
2022-11-30 13:36:01 +0200

    Set staging WP_HOME in config so it works on kinsta when restoring a backup

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -16,3 +16,5 @@ use Roots\WPConfig\Config;
  */
 
 Config::define('SHC_SHOW_ENV_STAGING', 'staging');
+Config::define('WP_HOME', 'https://staging-gdsbedrock-staging.kinsta.cloud');
+Config::define('WP_SITEURL', Config::get('WP_HOME') . '/wp');
