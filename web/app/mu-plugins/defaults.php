52592a5397c9c548cf4346cacd8abc55b5e1ae57
Oskar Schöldström <m@oxy.fi>
2022-12-04 14:55:02 -0300

    Remove emoji assets

--- web/app/mu-plugins/defaults.php
+++ web/app/mu-plugins/defaults.php
@@ -71,6 +71,24 @@ add_filter('xmlrpc_enabled', '__return_false');
 remove_action('init', 'smilies_init', 5);
 remove_filter('the_content', 'convert_smilies', 20);
 remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+add_action('init', function () {
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('wp_resource_hints', function (array $urls, string $relationType) {
+        if ($relationType === 'dns-prefetch') {
+            $urls = array_diff($urls, [
+                apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/'),
+            ]);
+        }
+        return $urls;
+    }, 10, 2);
+});
+
 
 /**
  * @see https://github.com/Automattic/wp-super-cache/issues/397
