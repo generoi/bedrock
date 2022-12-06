62ffedf33c7c53e3d9e8eb6136560eac1d4460e0
Oskar Schöldström <m@oxy.fi>
2022-12-06 07:47:49 -0300

    Add a toggle-button component

--- web/app/themes/wp-gds-theme/resources/scripts/app.js
+++ web/app/themes/wp-gds-theme/resources/scripts/app.js
@@ -1,4 +1,5 @@
 import swiper from './components/swiper'
+import './components/toggle-button';
 
 // app.js is loaded at the end of body, so don't wait for document ready.
 for (const container of document.body.querySelectorAll('.swiper-container')) {
---
+++ web/app/themes/wp-gds-theme/resources/scripts/components/toggle-button.js
@@ -0,0 +1,117 @@
+const EVENT_OPEN = 'open';
+const EVENT_CLOSE = 'close';
+
+/**
+ * @example
+ * <toggle-button class="toggler" aria-controls="submenu">
+ *   Open
+ * </toggle-button>
+ * <div class="submenu" id="submenu">
+ *   Content
+ * </div>
+ */
+export class ToggleButton extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.addEventListener(EVENT_OPEN, this.open.bind(this));
+    this.addEventListener(EVENT_CLOSE, this.close.bind(this));
+    this.addEventListener('keydown', this.handleKeyDown.bind(this));
+    this.addEventListener('click', this.toggle.bind(this));
+    this.controls.forEach((target) => {
+      target.addEventListener('focusout', this.handleTargetContainerBlur.bind(this, target));
+      target.addEventListener('keydown', this.handleTargetContainerKeyDown.bind(this));
+    });
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ['aria-expanded', 'aria-controls'];
+  }
+
+  get controls() {
+    let controls = this.getAttribute('aria-controls');
+    if (!controls) {
+      return [];
+    }
+    controls = controls.split(' ').map((target) => document.getElementById(target));
+    return controls;
+  }
+
+  get expanded() {
+    return this.getAttribute('aria-expanded') === 'true';
+  }
+
+  set expanded(value) {
+    this.dispatchEvent(
+      new CustomEvent(
+        value ? EVENT_OPEN : EVENT_CLOSE,
+        {cancelable: true, bubbles: true}
+      )
+    );
+  }
+
+  toggle() {
+    this.expanded = !this.expanded;
+  }
+
+  open() {
+    this.setAttribute('aria-expanded', true);
+    this.controls.forEach((target) => target.classList.add('is-active'))
+  }
+
+  close() {
+    this.setAttribute('aria-expanded', false);
+    this.controls.forEach((target) => target.classList.remove('is-active'))
+  }
+
+  handleKeyDown(event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      this.toggle();
+    }
+  }
+
+  handleTargetContainerKeyDown(event) {
+    if (event.key === 'Escape') {
+      this.expanded = false;
+    }
+  }
+
+  handleTargetContainerBlur(container, event) {
+    const newFocusElement = event.relatedTarget;
+    if (newFocusElement && !container.contains(newFocusElement)) {
+      this.close();
+    }
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          cursor: pointer;
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+        }
+      </style>
+      <slot/>
+    `;
+
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'button');
+    }
+    if (!this.hasAttribute('aria-expanded')) {
+      this.setAttribute('aria-expanded', 'false');
+    }
+    if (!this.hasAttribute('tabindex')) {
+      this.setAttribute('tabindex', '0');
+    }
+  }
+}
+
+customElements.define('toggle-button', ToggleButton);
