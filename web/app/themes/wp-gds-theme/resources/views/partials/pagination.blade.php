b3e4df1ee2b3e6d446295366d48705bc37b9bfd5
Oskar Schöldström <m@oxy.fi>
2022-12-13 08:31:07 -0300

    Style pagination

--- web/app/themes/wp-gds-theme/resources/views/partials/pagination.blade.php
+++ web/app/themes/wp-gds-theme/resources/views/partials/pagination.blade.php
@@ -1,14 +1,15 @@
 @if (isset($query) && $query->max_num_pages > 1)
-  <div class="pagination">
+  <nav class="pagination" aria-label="{{ __('Pagination') }}">
     {!! paginate_links([
       'base' => str_replace(999999, '%#%', esc_url(get_pagenum_link(999999))),
       'format' => '?paged=%#%',
       'current' => max(1, get_query_var('paged')),
       'total' => $query->max_num_pages,
       'type' => 'list',
-      'prev_text' => '<i class="fas fa-chevron-left fa-xs"></i>',
-      'next_text' => '<i class="fas fa-chevron-right fa-xs"></i>',
+      'before_page_number' => sprintf('<span class="sr-only">%s</span>', __('Page', 'gds')),
+      'prev_text' => sprintf('<i class="fas fa-chevron-left fa-xs"></i><span class="sr-only">%s</span>', __('Previous page', 'gds')),
+      'next_text' => sprintf('<i class="fas fa-chevron-right fa-xs"></i><span class="sr-only">%s</span>', __('Next page', 'gds')),
       'add_fragment' => !empty($fragment) ? "#$fragment" : null,
     ]) !!}
-  </div>
+  </nav>
 @endif
