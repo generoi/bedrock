6efadaeb2e31b0ac378efd1fced986cbf7095568
Oskar Schöldström <m@oxy.fi>
2023-07-17 11:18:24 -0300

    Folllowup 7720fba1: Missing WP_ENV ref

--- web/app/mu-plugins/disallow-indexing.php
+++ web/app/mu-plugins/disallow-indexing.php
@@ -12,6 +12,6 @@ License:      MIT License
 
 namespace Genero\Site;
 
-if (defined('WP_ENV') && WP_ENV !== 'production' && !is_admin()) {
+if (defined('WP_ENVIRONMENT_TYPE') && WP_ENVIRONMENT_TYPE !== 'production' && !is_admin()) {
     add_filter('pre_option_blog_public', '__return_zero');
 }
