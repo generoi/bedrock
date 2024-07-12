3cd6fc03cb7219b9f1a3ace568f8902a24fefd0a
Oskar Schöldström <m@oxy.fi>
2024-07-12 15:21:04 -0300

    Add transform between grid <> carousel <> columns

--- web/app/themes/gds/resources/blocks/grid/index.js
+++ web/app/themes/gds/resources/blocks/grid/index.js
@@ -1,6 +1,7 @@
 /** @wordpress */
 import { registerBlockType } from '@wordpress/blocks'
 import { InnerBlocks } from '@wordpress/block-editor'
+import { createBlock } from '@wordpress/blocks';
 
 import edit from './edit'
 import variations from './variations'
@@ -12,5 +13,43 @@ registerBlockType(meta.name, {
   variations,
   save() {
     return <InnerBlocks.Content />;
+  },
+  transforms: {
+    from: [
+      {
+        type: 'block',
+        blocks: ['core/columns'],
+        transform: (attributes, innerBlocks) => {
+          attributes.columns = innerBlocks.length;
+
+          return createBlock(
+            meta.name,
+            attributes,
+            innerBlocks.map((columnBlock) => {
+              if (columnBlock.innerBlocks.length === 1) {
+                return columnBlock.innerBlocks[0];
+              }
+              // Wrap it in a group
+              return createBlock('core/group', columnBlock.attributes, columnBlock.innerBlocks);
+            }),
+          );
+        },
+      },
+    ],
+    to: [
+      {
+        type: 'block',
+        blocks: ['core/columns'],
+        transform: (attributes, innerBlocks) => {
+          return createBlock(
+            'core/columns',
+            attributes,
+            innerBlocks.map((innerBlock) => {
+              return createBlock('core/column', {}, [innerBlock]);
+            }),
+          );
+        },
+      },
+    ],
   },
 });
