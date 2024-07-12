852ce4fd1d6beffe34a2b01a1f4e999b819a8d3d
Oskar Schöldström <m@oxy.fi>
2024-07-12 16:59:57 -0300

    Add better icons and examples

--- web/app/themes/gds/resources/blocks/carousel/variations.js
+++ web/app/themes/gds/resources/blocks/carousel/variations.js
@@ -1,7 +1,19 @@
+import { Path, SVG } from '@wordpress/components';
+
 const variations = [
   {
     name: 'basic',
     title: 'Basic',
+    icon: (
+      <SVG
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+      >
+        <Path d="M0 10a2 2 0 0 1 2-2h44a2 2 0 0 1 2 2v28a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V10Z" />
+      </SVG>
+    ),
     innerBlocks: [
       ['gds/carousel-item', {}, [
         ['core/paragraph', {}],
@@ -16,6 +28,16 @@ const variations = [
     name: 'three-media-cards',
     title: '3 Media Cards',
     description: 'A carousel displaying Media Cards 3 at a time',
+    icon: (
+      <SVG
+        xmlns="http://www.w3.org/2000/svg"
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+      >
+        <Path d="M0 10a2 2 0 0 1 2-2h10.531c1.105 0 1.969.895 1.969 2v28c0 1.105-.864 2-1.969 2H2a2 2 0 0 1-2-2V10Zm16.5 0c0-1.105.864-2 1.969-2H29.53c1.105 0 1.969.895 1.969 2v28c0 1.105-.864 2-1.969 2H18.47c-1.105 0-1.969-.895-1.969-2V10Zm17 0c0-1.105.864-2 1.969-2H46a2 2 0 0 1 2 2v28a2 2 0 0 1-2 2H35.469c-1.105 0-1.969-.895-1.969-2V10Z" />
+      </SVG>
+    ),
     innerBlocks: [
       ['gds/carousel-item', {templateLock: 'all'}, [
         ['gds/media-card', {lock:{remove: true}}],
