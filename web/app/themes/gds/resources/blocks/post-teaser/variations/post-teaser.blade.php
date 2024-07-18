115c8cccdcb97064411e48591ccb485de8e91f6a
Oskar Schöldström <m@oxy.fi>
2024-07-18 08:50:05 -0300

    Re-use media-card block in teaser

--- web/app/themes/gds/resources/blocks/post-teaser/variations/post-teaser.blade.php
+++ web/app/themes/gds/resources/blocks/post-teaser/variations/post-teaser.blade.php
@@ -4,25 +4,16 @@
     'wp-block-gds-post-teaser--' . get_post_type(),
   ])->filter()->join(' ')
 ]) !!}>
-  <figure class="wp-block-gds-post-teaser__media">
-    @if (has_post_thumbnail())
-      @block('core/post-featured-image', ['sizeSlug' => 'medium'])
-    @else
-      <img src="{{ Roots\asset('images/default-teaser.webp') }}" alt="" />
-    @endif
-  </figure>
-  <div class="wp-block-gds-post-teaser__content">
-    @block('core/post-title', ['level' => 3])
-    @block('core/post-date', ['format' => 'd.m.Y'])
-    @block('core/post-excerpt', [])
-
-    @block('core/post-terms', [
-      'term' => 'category',
-      'separator' => '',
-    ])
-
-    @block('core/read-more', [
-      'content' => __('Read article', 'gds') . get_svg('icons.solid.chevron-right', '2xs')
-    ])
-  </div>
+  @blocks
+    <!-- wp:gds/media-card @json([
+      'useFeaturedImage' => has_post_thumbnail(),
+      'mediaUrl' => Roots\asset('images/default-teaser.webp')->uri()
+    ]) -->
+      <!-- wp:core/post-title {"level": 3} /-->
+      <!-- wp:core/post-date {"format": "d.m.Y"} /-->
+      <!-- wp:core/post-excerpt /-->
+      <!-- wp:core/post-terms {"term": "category", "separator": ""} /-->
+      <!-- wp:core/read-more {"content": "{{ __('Read article', 'gds') }}"} /-->
+    <!-- /wp:gds/media-card -->
+  @endblocks
 </div>
