7093bcad3247e930024d5c5ad9f4cf289faf7b70
Oskar Schöldström <m@oxy.fi>
2024-07-18 13:35:31 -0300

    Add page-title block instead of blade partial

---
+++ web/app/themes/gds/resources/blocks/page-title/page-title.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace blocks\page_title;
+
+use WP_Block;
+
+register_block_type(asset('blocks/page-title/block.json')->path(), [
+    'render_callback' => function (array $attributes, string $content = '', $isPreview = false, int $postId = 0, ?WP_Block $block = null, array|bool $context = false) {
+        $attributes = (object) $attributes;
+
+        echo view('blocks::page-title.page-title', [
+            'is_preview' => is_bool($isPreview) ? $isPreview : false,
+        ]);
+    }
+]);
