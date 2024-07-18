c7497402ca562e95475994075790e51c740966c1
Oskar Schöldström <m@oxy.fi>
2024-07-18 20:05:24 -0300

    Setup product post-teaser

--- web/app/themes/gds/resources/blocks/post-teaser/variations/product.blade.php
+++ web/app/themes/gds/resources/blocks/post-teaser/variations/product.blade.php
@@ -1,1 +1,17 @@
-@extends('blocks::post-teaser.variations.post-teaser')
+<div {!! get_block_wrapper_attributes([
+  'class' => collect([
+    ! empty($attributes->textAlign) ? sprintf('has-text-align-%s', $attributes->textAlign) : '',
+    'wp-block-gds-post-teaser--' . get_post_type(),
+  ])->filter()->join(' ')
+]) !!}>
+  @blocks
+    <!-- wp:gds/media-card @json([
+      'useFeaturedImage' => has_post_thumbnail(),
+      'mediaUrl' => Roots\asset('images/default-teaser.webp')->uri()
+    ]) -->
+      <!-- wp:core/post-title {"level": 3,"isLink": true} /-->
+      <!-- wp:woocommerce/product-price /-->
+      <!-- wp:core/read-more {"content": "{{ __('Read more', 'gds') }}"} /-->
+    <!-- /wp:gds/media-card -->
+  @endblocks
+</div>
