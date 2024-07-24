8a19ee37a3a7720de369e61ee902423104be5523
Oskar Schöldström <m@oxy.fi>
2024-07-24 09:28:15 -0300

    Allow biometric-authentication plugin rest api endpoint

--- web/app/mu-plugins/disable-rest-api.php
+++ web/app/mu-plugins/disable-rest-api.php
@@ -28,7 +28,7 @@ add_filter('rest_authentication_errors', function ($access) {
 
     // Allow logged out users access to the following endpoints, prevent access
     // to others.
-    if (preg_match('@^/(facetwp|gds|wc)/@', $route)) {
+    if (preg_match('@^/(facetwp|gds|wc|wp-passkey)/@', $route)) {
         return $access;
     }
 
